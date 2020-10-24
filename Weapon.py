eff60e77b97bda2fe7fd389a0414a5696b90bd27
Matikmat <73387931+Matikmat@users.noreply.github.com>
2020-10-24 23:11:37 +0200

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -18,15 +18,37 @@ class Bullet:
         bullets_enemy.append(Bullet(posx1,posy2))
 
     def make_bullet_player(self, bullets_player, posx1, posy1):
-       bullets_player.append(Bullet(posx1,posy1))
+        bullets_player.append(Bullet(posx1,posy1))
         bullets_player.append(Bullet(posx1+30, posy1))
 
-    def delete_bullet(self, bullets_player, bullets_enemy):
+    def bullets_sollision_check(self, bullets_player, bullets_enemy, enemy_list):
         for i in bullets_player:
             if i.posy - Bullet.height <= 0:
                 bullets_player.pop(i)
+
         for i in bullets_enemy:
             if i.posy - Bullet.height >= 1960:
                 bullets_enemy.pop(i)
 
-        for i in
+        for i in len(enemy_list): #enemy hit
+        if bullets_player[i].posx - Bullet.width <= enemy_list[i].posx - enemy_list[i].width: # warunek na posx1
+            if bullets_player[i].posx + Bullet.width >= enemy_list[i].posx - enemy_list[i].width:
+                if bullets_player[i].posy + Bullet.height >= enemy_list[i].posy + enemy_list[i].height:  # warunek na posy1
+                    if bullets_player[i].posy - Bullet.height <= enemy_list[i].posy + enemy_list[i].height:
+                        enemy_list[i].hit = 1
+                if bullets_player[i].posy - Bullet.height <= enemy_list[i].posy - enemy_list[i].height:  # warunek na posy2
+                    if bullets_player[i].posy + Bullet.height >= enemy_list[i].posy - enemy_list[i].height:
+                        enemy_list[i].hit = 1
+
+
+        if bullets_player[i].posx + Bullet.width >= enemy_list[i].posx + enemy_list[i].width: # warunek na posx2
+            if bullets_player[i].posx - Bullet.width <= enemy_list[i].posx + enemy_list[i].width:
+                if bullets_player[i].posy + Bullet.height >= enemy_list[i].posy + enemy_list[i].height:  # warunek na posy1
+                    if bullets_player[i].posy - Bullet.height <= enemy_list[i].posy + enemy_list[i].height:
+                        enemy_list[i].hit = 1
+                if bullets_player[i].posy - Bullet.height <= enemy_list[i].posy - enemy_list[i].height:  # warunek na posy2
+                    if bullets_player[i].posy + Bullet.height >= enemy_list[i].posy - enemy_list[i].height:
+                        enemy_list[i].hit = 1
+
+
+
