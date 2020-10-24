fd44ffada7aba4ee50d9aac0a553b570bb494fe2
KiteTheHunter <58311054+KiteTheHunter@users.noreply.github.com>
2020-10-24 22:20:04 +0200

    Update Window.py

--- Window.py
+++ Window.py
@@ -1,14 +1,11 @@
 import pygame
 
 class Window(object):
-    def  __init__(self, width,  height, clock):
+    def  __init__(self, width,  height):
         self.width  =  width
         self.height = height
-        self.clock = clock
         self.win =  pygame.display.set_mode((width,  height))
-        self.clock = pygame.time.Clock
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption("Pythoneers++:  Attack on CO2")
-    def draw(self, image, position, area):
-        self.win.blit(image, position, area=area)
     def update(self):
         pygame.display.update()
