1ed8d51812301e1f0862fc1af6ff1ccc9c703e01
Matikmat <73387931+Matikmat@users.noreply.github.com>
2020-10-24 21:59:56 +0200

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -1,10 +1,11 @@
 import pygame, os
-class Bullets:
-    def self__init__(self,posx,posy,width,height):
+class Bullet:
+    def self__init__(self, posx, posy, width, height, texture):
         self.posx = posx
         self.posy = posy
         self.width = 5
         self.height = 25
+        self.texture = pygame.image.load(os.path.join('textures','bullet.png'))
 
 class Weapon:
     def self__init__(self,ats,dmg):
@@ -12,8 +13,10 @@ class Weapon:
         self.dmg = 1
 
     bullets_player = []
-    bullets_enemies = []
-    def make_bullet_enemey(self):
+    bullets_enemy = []
+    def make_bullet_enemey(self, bullets_enemy, pos1, pos2):
+        bullets_enemy.append(Bullet(pos1,pos2))
 
-    def make_bullet_player(self):
-    1
+    def make_bullet_player(self, bullets_player, posx1, posy1):
+        bullets_player.append(Bullet(posx1,posy1))
+        bullets_player.append(Bullet(posx1+30, posy1))
