aaec5bfbc4926f03e53b95d42549f998f7dc222b
Seronegani <73401791+Seronegani@users.noreply.github.com>
2020-10-25 09:53:34 +0100

    Update World.py

--- World.py
+++ World.py
@@ -7,12 +7,15 @@ class World:
         self.window = surface.win
         self.bullets_player = []
         self.bullets_enemy = []
+        self.drawIteration = 0
+        self.background = pygame.image.load('Atmosphere.png')
+        self.background = pygame.transform.scale(self.background, (int(self.background.get_width() * 2.9), int(self.background.get_height() * 2.9)))
     def update(self, time):
         pass
 
     def draw(self, win):
-        background = pygame.image.load('Atmosphere.png')
-        win.blit(pygame.transform.scale(background, (int(background.get_width() * 8), int(background.get_height() * 8))), (0, 0))
+        self.drawIteration += 1
+        win.blit(self.background, (0, -33408 + 1920 + (int(self.drawIteration * 30)) % 21500))
 
     def make_bullet_enemy(self, bullets_enemy, posx1, posy2):
         bullets_enemy.append(Weapon (posx1, posy2))
