a1ce8450b62983f0b639eebadcdd7a5fcd92bc35
Matikmat <73387931+Matikmat@users.noreply.github.com>
2020-10-25 08:15:10 +0100

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -1,9 +1,9 @@
 import pygame, os
 
 class Weapon:
-    def self__init__(self, posx, posy,image):
-        self.posx = posx
-        self.posy = posy
+    def self__init__(self, x, y,image):
+        self.x = x
+        self.y = y
         self.width = 5
         self.height = 25
         self.image = image
@@ -26,54 +26,63 @@ class Weapon:
         bullets_player.append(Weapon(posx1, posy1, texture1))
         bullets_player.append(Weapon(posx1 + 30, posy1, texture1))
 
-    def bullets_sollision_check(self, bullets_player, bullets_enemy, enemy_list, Player.posx, Player.posy):
+    def bullets_sollision_check(self, bullets_player, bullets_enemy, enemy_list, PlayerCharacter.x, PlayerCharacter.y):
         for i in bullets_player:
-            if i.posy - Weapon.height <= 0:
+            if i.y - Weapon.height <= 0:
                 bullets_player.pop(i)
                 bullets_enemy[i].delete
 
         for i in bullets_enemy:
-            if i.posy - Weapon.height >= 1920:
+            if i.y - Weapon.height >= 1920:
                 bullets_enemy.pop(i)
                 bullets_enemy[i].delete
 
         for i in len(enemy_list): #enemy hit
-            if bullets_player[i].posx - Weapon.width <= enemy_list[i].posx - enemy_list[i].width: # warunek na posx1
-                if bullets_player[i].posx + Weapon.width >= enemy_list[i].posx - enemy_list[i].width:
-                    if bullets_player[i].posy + Weapon.height >= enemy_list[i].posy + enemy_list[i].height:  # warunek na posy1
-                        if bullets_player[i].posy - Weapon.height <= enemy_list[i].posy + enemy_list[i].height:
+            if bullets_player[i].x - Weapon.width <= enemy_list[i].x - enemy_list[i].width: # warunek na posx1
+                if bullets_player[i].x + Weapon.width >= enemy_list[i].x - enemy_list[i].width:
+                    if bullets_player[i].y + Weapon.height >= enemy_list[i].y + enemy_list[i].height:  # warunek na posy1
+                        if bullets_player[i].y - Weapon.height <= enemy_list[i].y + enemy_list[i].height:
                             enemy_list[i].hit = 1
-                    if bullets_player[i].posy - Weapon.height <= enemy_list[i].posy - enemy_list[i].height:  # warunek na posy2
-                        if bullets_player[i].posy + Weapon.height >= enemy_list[i].posy - enemy_list[i].height:
+                            return 0
+                    if bullets_player[i].y - Weapon.height <= enemy_list[i].y - enemy_list[i].height:  # warunek na posy2
+                        if bullets_player[i].y + Weapon.height >= enemy_list[i].y - enemy_list[i].height:
                             enemy_list[i].hit = 1
+                            return 0
 
 
-            if bullets_player[i].posx + Weapon.width >= enemy_list[i].posx + enemy_list[i].width: # warunek na posx2
-                if bullets_player[i].posx - Weapon.width <= enemy_list[i].posx + enemy_list[i].width:
-                    if bullets_player[i].posy + Weapon.height >= enemy_list[i].posy + enemy_list[i].height:  # warunek na posy1
-                        if bullets_player[i].posy - Weapon.height <= enemy_list[i].posy + enemy_list[i].height:
+            if bullets_player[i].x + Weapon.width >= enemy_list[i].x + enemy_list[i].width: # warunek na posx2
+                if bullets_player[i].x - Weapon.width <= enemy_list[i].x + enemy_list[i].width:
+                    if bullets_player[i].y + Weapon.height >= enemy_list[i].y + enemy_list[i].height:  # warunek na posy1
+                        if bullets_player[i].y - Weapon.height <= enemy_list[i].y + enemy_list[i].height:
                             enemy_list[i].hit = 1
-                    if bullets_player[i].posy - Weapon.height <= enemy_list[i].posy - enemy_list[i].height:  # warunek na posy2
-                        if bullets_player[i].posy + Weapon.height >= enemy_list[i].posy - enemy_list[i].height:
+                            return 0
+                    if bullets_player[i].y - Weapon.height <= enemy_list[i].y - enemy_list[i].height:  # warunek na posy2
+                        if bullets_player[i].y + Weapon.height >= enemy_list[i].y - enemy_list[i].height:
                             enemy_list[i].hit = 1
+                            return 0
 
 
             # player hit
-        if bullets_enemy[i].posx - Weapon.width <= Player.posx - Player.width: # warunek na posx1
-            if bullets_enemy[i].posx + Weapon.width >= Player.posx - Player.width:
-                if bullets_enemy[i].posy + Weapon.height >= Player.posy + Player.height:  # warunek na posy1
-                    if bullets_enemy[i].posy - Weapon.height <= Player.posy + Player.height:
+        if bullets_enemy[i].x - Weapon.width <= Player.x - Player.width: # warunek na posx1
+            if bullets_enemy[i].x + Weapon.width >= Player.x - Player.width:
+                if bullets_enemy[i].y + Weapon.height >= Player.y + Player.height:  # warunek na posy1
+                    if bullets_enemy[i].y - Weapon.height <= Player.y + Player.height:
                         Player.hit = 1
-                if bullets_enemy[i].posy - Weapon.height <= Player[i].posy - Player[i].height:  # warunek na posy2
-                    if bullets_enemy[i].posy + Weapon.height >= Player[i].posy - Player[i].height:
+                        return 0
+                if bullets_enemy[i].y - Weapon.height <= Player[i].y - Player[i].height:  # warunek na posy2
+                    if bullets_enemy[i].y + Weapon.height >= Player[i].y - Player[i].height:
                         Player.hit = 1
+                        return 0
 
 
-        if bullets_enemy[i].posx + Weapon.width >= Player.posx + Player.width: # warunek na posx2
-            if bullets_enemy[i].posx - Weapon.width <= Player.posx + Player.width:
-                if bullets_enemy[i].posy + Weapon.height >= Player.posy + Player.height:  # warunek na posy1
-                    if bullets_enemy[i].posy - Weapon.height <= Player.posy + Player.height:
+        if bullets_enemy[i].x + Weapon.width >= Player.x + Player.width: # warunek na posx2
+            if bullets_enemy[i].x - Weapon.width <= Player.x + Player.width:
+                if bullets_enemy[i].y + Weapon.height >= Player.y + Player.height:  # warunek na posy1
+                    if bullets_enemy[i].y - Weapon.height <= Player.y + Player.height:
                         Player.hit = 1
-                if bullets_enemy[i].posy - Weapon.height <= Player.posy - Player.height:  # warunek na posy2
-                    if bullets_enemy[i].posy + Weapon.height >= Player.posy - Player.height:
+                        return 0
+                if bullets_enemy[i].y - Weapon.height <= Player.y - Player.height:  # warunek na posy2
+                    if bullets_enemy[i].y + Weapon.height >= Player.y - Player.height:
                         Player.hit = 1
+                        return 0
+        return 0
